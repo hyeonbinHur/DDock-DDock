d1ac837169ca0c2c5a20bf6e4dee5ddef96836b7
hyeonbinHur <160996936+hyeonbinHur@users.noreply.github.com>
2024-03-26 20:56:31 +0900

    basic settings (including firebase)
    npm install react-router-dom
    npm install firebase

---
+++ DDock-DDock/src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBUnhxHBFK_fIBBuEu4p1LCXBSuxIMB8NM",
+    authDomain: "ddock-ddock.firebaseapp.com",
+    projectId: "ddock-ddock",
+    storageBucket: "ddock-ddock.appspot.com",
+    messagingSenderId: "212572232674",
+    appId: "1:212572232674:web:ad7d41106c12981030f7bb"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+//
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, timestamp };
