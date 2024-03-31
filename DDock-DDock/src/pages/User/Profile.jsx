f1b018717413f42736c42313f1b6bc0e8fe54e0b
hyeonbinHur <160996936+hyeonbinHur@users.noreply.github.com>
2024-04-01 03:58:31 +0900

    get one user info

--- DDock-DDock/src/pages/User/Profile.jsx
+++ DDock-DDock/src/pages/User/Profile.jsx
@@ -1,12 +1,21 @@
-export default function ProfilePage(){
+import { useParams } from 'react-router-dom';
+import { useDocument } from '../../hooks/useDocument';
+import { useCollection } from '../../hooks/useCollection';
+
+export default function ProfilePage() {
+    const { userId } = useParams();
+    // const {document} = useCollection('User');
+    const { document, error, loading } = useDocument('User', userId);
+    console.log(document);
+    console.log(document);
     return (
         <div>
             <label>Hlloe Profile</label>
-            <label></label>
+            {document && <label>{document.displayName}</label>}
             <div>----M ITEM----</div>
             <div>----H ITEM----</div>
             <div>----J ITEM----</div>
             <div>----C ITEM----</div>
         </div>
-    )
+    );
 }
