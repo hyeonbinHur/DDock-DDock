ef0197d8e8df00aeb6ac04e9929df4df550ebc10
hyeonbinHur <160996936+hyeonbinHur@users.noreply.github.com>
2024-04-02 04:42:01 +0900

    upload image success

--- DDock-DDock/src/pages/Memo.jsx
+++ DDock-DDock/src/pages/Memo.jsx
@@ -2,18 +2,17 @@ import defaultImg from '../assets/default.png';
 import style from './memo.module.css';
 import { projectStorage } from '../firebase/config';
 import { useEffect, useState } from 'react';
-import { v4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'; // uuid 라이브러리에서 v4 함수를 uuidv4라는 이름으로 가져옵니다.
 
 export default function Memo() {
     const [imageUpload, setImageUpload] = useState(null);
     const [imageUrls, setImageUrls] = useState([]);
 
- 
     const uploadImage = () => {
-        if (imageUpload == null) return;
-        const imageRef = projectStorage.ref(`images/${imageUpload.name + v4()}`);
-        projectStorage.uploadBytes(imageRef, imageUpload).then((snapshot) => {
-            projectStorage.ref(snapshot.ref.fullPath).getDownloadURL().then((url) => {
+        if (!imageUpload) return; // 파일이 선택되지 않았다면 아무 작업도 하지 않음
+        const imageRef = projectStorage.ref(`images/${imageUpload.name}_${uuidv4()}`);
+        imageRef.put(imageUpload).then(() => {
+            imageRef.getDownloadURL().then((url) => {
                 setImageUrls((prev) => [...prev, url]);
             });
         });
@@ -35,15 +34,14 @@ export default function Memo() {
             <input
                 type="file"
                 onChange={(event) => {
-                    setImageUpload(event.target.files[0]);
+                    setImageUpload(event.target.files[0]); // 이벤트 대상의 files 배열에서 첫 번째 파일을 선택
                 }}
             />
             <button onClick={uploadImage}>submit image</button>
-            <img className={style.image} src={defaultImg} />
-
-            {imageUrls.map((url) =>{
-                return <img src={url} key={url}/>;
-            })}
+            <img className={style.image} src={defaultImg} alt="Default" />
+            {imageUrls.map((url) => (
+                <img src={url} alt="" key={url} />
+            ))}
         </div>
     );
 }
