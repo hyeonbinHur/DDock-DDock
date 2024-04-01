62f0c3bc37b9b3ccb61a933b441f51d5fd5470df
hyeonbinHur <160996936+hyeonbinHur@users.noreply.github.com>
2024-04-02 04:37:07 +0900

    start to upload image

--- DDock-DDock/src/pages/Memo.jsx
+++ DDock-DDock/src/pages/Memo.jsx
@@ -1,28 +1,49 @@
 import defaultImg from '../assets/default.png';
 import style from './memo.module.css';
 import { projectStorage } from '../firebase/config';
-import { useState } from 'react';
-import {ref} from 'firebase/storage'
-
+import { useEffect, useState } from 'react';
+import { v4 } from 'uuid';
 
 export default function Memo() {
+    const [imageUpload, setImageUpload] = useState(null);
+    const [imageUrls, setImageUrls] = useState([]);
 
-    const [imageUpload,setImageUpload] = useState(null);
-    const imageListRef = ref (projectStorage, "images/");
+ 
+    const uploadImage = () => {
+        if (imageUpload == null) return;
+        const imageRef = projectStorage.ref(`images/${imageUpload.name + v4()}`);
+        projectStorage.uploadBytes(imageRef, imageUpload).then((snapshot) => {
+            projectStorage.ref(snapshot.ref.fullPath).getDownloadURL().then((url) => {
+                setImageUrls((prev) => [...prev, url]);
+            });
+        });
+    };
 
-    const uploadImage = () =>{
+    useEffect(() => {
+        const imageListRef = projectStorage.ref('images/');
+        imageListRef.listAll().then((response) => {
+            response.items.forEach((item) => {
+                item.getDownloadURL().then((url) => {
+                    setImageUrls((prev) => [...prev, url]);
+                });
+            });
+        });
+    }, []);
 
-        if(imageUpload === null) return;
+    return (
+        <div>
+            <input
+                type="file"
+                onChange={(event) => {
+                    setImageUpload(event.target.files[0]);
+                }}
+            />
+            <button onClick={uploadImage}>submit image</button>
+            <img className={style.image} src={defaultImg} />
 
-        const imageRef = ref(projectStorage, `images/${imageUpload.name}`)
-    }
-
-    return (<div>
-        <input type="file"
-        onChange={(event) => {
-            setImageUpload(event.target.value)
-        }}/>
-        <button onClick={uploadImage}>submit image</button>
-        <img className={style.image} src={defaultImg} />
-    </div>);
+            {imageUrls.map((url) =>{
+                return <img src={url} key={url}/>;
+            })}
+        </div>
+    );
 }
