f21497328ba7218f42c131f25eda5f81aa5deaf2
hyeonbinHur <160996936+hyeonbinHur@users.noreply.github.com>
2024-05-01 05:53:22 +0900

    job detail., delete done

--- src/pages/Job/AddJobPage.jsx
+++ src/pages/Job/AddJobPage.jsx
@@ -2,10 +2,11 @@ import { useFirestore } from '../../hooks/useFirestore';
 import { useAuthContext } from '../../hooks/useAuth';
 import { getSydneyTimeISO } from '../../util/formDate';
 import { useDocument } from '../../hooks/useDocument';
-import JobItemAddForm from '../../components/JobItem/JobItemAddForm';
+// import JobItemAddForm from '../../components/JobItem/JobItemAddForm';
+import ItemAddForm from '../../components/Common/ItemAddForm';
 
 export default function AddJobPage() {
-    const { addDocument, response, loading } = useFirestore('JobItem');
+    const { addDocument, response } = useFirestore('JobItem');
     const { user } = useAuthContext();
     const { document: userData } = useDocument('User', user?.uid);
 
@@ -29,16 +30,28 @@ export default function AddJobPage() {
                     gu: userData.location.gu,
                     dong: userData.location.dong,
                 },
-                comments:[],
+                comments: [],
                 createdAt,
                 userId: userData.id,
                 type: 'J_Item',
                 interests: 0,
                 numOfComment: 0,
             };
-            await addDocument(newItem, "jobItem");
+            await addDocument(newItem, 'jobItem');
         }
     };
 
-    return <> {userData && <JobItemAddForm addDocumentToServer = {addDocumentToServer} response = {response} loading = {loading} />} </>;
+    return (
+        <>
+            {' '}
+            {userData && (
+                <ItemAddForm
+                    addDocumentToServer={addDocumentToServer}
+                    response={response}
+                    Topic={'Job'}
+                    condition={true}
+                />
+            )}{' '}
+        </>
+    );
 }
