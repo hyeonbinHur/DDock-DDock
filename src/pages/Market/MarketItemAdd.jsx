6070cda6aed76ae5c530d7ea0f2cdafdb38c2737
hyeonbinHur <160996936+hyeonbinHur@users.noreply.github.com>
2024-05-01 05:49:06 +0900

    market detail, delete done

--- src/pages/Market/MarketItemAdd.jsx
+++ src/pages/Market/MarketItemAdd.jsx
@@ -1,19 +1,26 @@
 import { useFirestore } from '../../hooks/useFirestore';
-import MarketItemForm from '../../components/MarketItem/MarketItemForm';
+// import MarketItemForm from '../../components/MarketItem/MarketItemForm';
 import { useAuthContext } from '../../hooks/useAuth';
 import { getSydneyTimeISO } from '../../util/formDate';
+import ItemAddForm from '../../components/Common/ItemAddForm';
 
 import { useDocument } from '../../hooks/useDocument';
 
-export default function AddMarketItem(){
-    const { addDocument, response, loading} = useFirestore('MarketItem');
-    const {user} = useAuthContext();
-    const {document: userInfo} = useDocument('User', user?.uid)
-    
-    //유저 업데이트 함수를 불러와서, 아이템 정보를 몽땅 저장 
+export default function AddMarketItem() {
+    const { user } = useAuthContext();
+    const { document: userData } = useDocument('User', user?.uid);
+    const { addDocument, response } = useFirestore('MarketItem');
 
-    const doAddDocument = async (title,description,images,bucket) => {
-        if(userInfo) {
+    //유저 업데이트 함수를 불러와서, 아이템 정보를 몽땅 저장
+
+    const addDocumentToServer = async (
+        title,
+        conditions,
+        description,
+        images,
+        bucket
+    ) => {
+        if (userData) {
             const createdAt = getSydneyTimeISO();
             const newMItem = {
                 title,
@@ -22,25 +29,31 @@ export default function AddMarketItem(){
                 images,
                 bucket,
                 location: {
-                    si: userInfo.location.si,
-                    gu: userInfo.location.gu,
-                    dong: userInfo.location.dong,
+                    si: userData.location.si,
+                    gu: userData.location.gu,
+                    dong: userData.location.dong,
                 },
                 createdAt: createdAt,
-                userId: userInfo.id,
-                type: "M_Item",
-                subtype: "M_Item",
+                userId: userData.id,
+                type: 'M_Item',
+                subtype: 'M_Item',
                 interests: 0,
-                numOfComment:0
-            }
-            await addDocument(newMItem,"marketItem");
+                numOfComment: 0,
+            };
+            await addDocument(newMItem, 'marketItem');
         }
-     
     };
 
     return (
         <>
-        <MarketItemForm doAction={doAddDocument} response={response} loading={loading}/>
+            {userData && (
+                <ItemAddForm
+                    addDocumentToServer={addDocumentToServer}
+                    response={response}
+                    Topic={'Market'}
+                    condition={false}
+                />
+            )}
         </>
-    );   
+    );
 }
--- src/pages/Market/MarketItemDetail.jsx
+++ src/pages/Market/MarketItemDetail.jsx
@@ -2,9 +2,9 @@ import { Link, useParams } from 'react-router-dom';
 // import { useDocument } from '../../hooks/useDocument';
 import Comment from '../../components/Comment/Comment';
 import { useAuthContext } from '../../hooks/useAuth';
-import style from './MarketItemDetail.module.css';
-import rightArrow from '../../assets/right.png';
-import leftArrow from '../../assets/left.png';
+// import style from './MarketItemDetail.module.css';
+// import rightArrow from '../../assets/right.png';
+// import leftArrow from '../../assets/left.png';
 import { useState, useEffect, useRef } from 'react';
 import ItemDeleteModal from '../../components/Modal/ItemDeleteModal';
 import { useNavigate } from 'react-router-dom';
@@ -12,9 +12,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getDocument } from '../../api/getDocument';
 import { readItem } from '../../store/ItemSlice';
 
+import { calculateTime } from '../../util/formDate';
+import { formDate2 } from '../../util/formDate';
+import { useDocument } from '../../hooks/useDocument';
+
 export default function MarketItemDetail() {
     const { mitemId } = useParams();
-
     // const { document, error } = useDocument('MarketItem', mitemId);
     const dispatch = useDispatch();
     const { user } = useAuthContext();
@@ -22,12 +25,16 @@ export default function MarketItemDetail() {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const modal = useRef();
-
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
-
     const reduxItem = useSelector((state) => state.itemInRedux.item);
+
+    const { year, month, day } = formDate2(reduxItem?.createdAt);
+    const { result: timeDif, unit: timeString } = calculateTime(
+        reduxItem?.createdAt
+    );
+    const { document: writer } = useDocument('User', reduxItem?.userId);
 
     useEffect(() => {
         if (mitemId) {
@@ -64,76 +71,141 @@ export default function MarketItemDetail() {
         return <div className="loading">Loading...</div>;
     }
 
-    const currentIndexMinus = () => {
-        setCurrentIndex((prev) => prev - 1);
-    };
-    const currentIndexPlus = () => {
-        setCurrentIndex((prev) => prev + 1);
-    };
+    // const currentIndexMinus = () => {
+    //     setCurrentIndex((prev) => prev - 1);
+    // };
+    // const currentIndexPlus = () => {
+    //     setCurrentIndex((prev) => prev + 1);
+    // };
     // const handleImageLoad = () => {
     //     setLoading(false);
     // };
-    function openConfirmModal() {
-        modal.current.open();
-    }
+    // function openConfirmModal() {
+    //     modal.current.open();
+    // }
 
     return (
         <>
-            {!isLoading && reduxItem && (
-                <div>
-                    <p>{reduxItem.title}</p>
+            {!error ? (
+                !isLoading && reduxItem && writer ? (
+                    <div className="pt-36 lg:flex lg:flex-col lg:items-center lg:justify-center">
+                        {/* images */}
+                        <div className="space-y-6 w-full h-2/3 ">
+                            <div className="flex flex-cols items-center justify-center h-5/6 w-full">
+                                {currentIndex > 0 && (
+                                    <button
+                                        onClick={() =>
+                                            setCurrentIndex((prev) => prev - 1)
+                                        }
+                                    >
+                                        prev
+                                    </button>
+                                )}
 
+                                <img
+                                    src={imageUrls[currentIndex]}
+                                    className="rounded-lg w-2/3 h-full lg:w-1/3"
+                                />
 
-                    <div className={style.imageContainer}>
-                        {imageUrls[currentIndex - 1] && (
-                            <img
-                                src={leftArrow}
-                                className={style.left}
-                                onClick={currentIndexMinus}
-                            />
-                        )}
-
-                        <div className={style.userImageContainer}>
-                            <img
-                                src={imageUrls[currentIndex]}
-                                className={style.mItemImage}
-                            />
+                                {currentIndex + 1 < imageUrls.length && (
+                                    <button
+                                        onClick={() =>
+                                            setCurrentIndex((prev) => prev + 1)
+                                        }
+                                    >
+                                        next
+                                    </button>
+                                )}
+                            </div>
+                            <div className="flex items-center justify-center font-bold">
+                                {currentIndex + 1}/{imageUrls.length}
+                            </div>
                         </div>
-                        {imageUrls[currentIndex + 1] && (
-                            <img
-                                src={rightArrow}
-                                className={style.right}
-                                onClick={currentIndexPlus}
-                            />
-                        )}
+                        {/* text div*/}
+                        <div className="w-full space-y-5 px-24 lg:w-1/3 lg:px-0">
+                            {/* writer */}
+                            <div className="flex h-28 justify-between">
+                                <div className="flex items-center space-x-5 space-y-3">
+                                    <img
+                                        src={writer.Avatar}
+                                        className="rounded-full h-20 w-20"
+                                    />
+                                    <div className="font-bold text-xl">
+                                        {writer.displayName}
+                                    </div>
+                                </div>
+                                <div className="font-light text-sm  grid grid-cols-1 place-items-end ">
+                                    {user?.uid == reduxItem.userId && (
+                                        <div className="space-y-2 w-5/12">
+                                            <div className="w-full border rounded flex justify-center items-center border-sky-300 bg-sky-200 hover:scale-105 hover:text-sky-600">
+                                                <Link className="" to={`edit`}>
+                                                    Edit
+                                                </Link>
+                                            </div>
+
+                                            <div
+                                                className="border border-red-200 bg-red-100 hover:scale-105 hover:text-red-600 text-center rounded-sm"
+                                                onClick={() =>
+                                                    modal.current.open()
+                                                }
+                                            >
+                                                Delete
+                                            </div>
+                                        </div>
+                                    )}
+                                    <div className="flex">
+                                        <div>
+                                            {month}, {day}, {year}
+                                        </div>
+                                        <div>
+                                            {timeDif} {timeString}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="border"></div>
+                            {/* location */}
+                            <div className="font-light text-sm flex space-x-10">
+                                <div>{reduxItem.location.gu}</div>
+                                <div>{reduxItem.location.dong}</div>
+                            </div>
+                            <div className="border"></div>
+
+                            {/* title layout */}
+                            <div className="font-bold text-lg ">
+                                <h1>{reduxItem.title}</h1>
+                            </div>
+
+                            {/* description layout */}
+                            <div className="">
+                                <div className="">{reduxItem.description}</div>
+                            </div>
+                            <div className="border"></div>
+
+                            <div>
+                                <Comment
+                                    serverItem={reduxItem}
+                                    collection="MarketItem"
+                                />
+                            </div>
+                        </div>
+
+                        {/* comment Layout */}
+
+                        <ItemDeleteModal
+                            ref={modal}
+                            id={mitemId}
+                            navigate={navigate}
+                            from={'market'}
+                            collection="MarketItem"
+                        />
                     </div>
-                    <p>
-                        {currentIndex + 1}/{imageUrls.length}
-                    </p>
-                    {user && user.uid === reduxItem.userId && (
-                        <div>
-                            <Link to={`/market/${mitemId}/mupdate`}>
-                                Go to edit
-                            </Link>
-                            <button onClick={() => openConfirmModal()}>
-                                delete Item
-                            </button>
-                            {/* <button
-                                onClick={() => dispatch(addCommentOnItem())}
-                            >
-                                Hello add Comment
-                            </button> */}
-                        </div>
-                    )}
-                    <Comment serverItem={reduxItem} collection="MarketItem" />
-                    <ItemDeleteModal
-                        ref={modal}
-                        id={mitemId}
-                        navigate={navigate}
-                        from={'market'}
-                        collection='MarketItem'
-                    />
-                </div>
+                ) : (
+                    <p>Loading</p>
+                )
+            ) : (
+                <p>Hello Error</p>
             )}
         </>
     );
